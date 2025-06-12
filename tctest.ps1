0fd26051057b56f83e5e37b3a35f99305168d01c
dawidbieniek <dawid_b01@wp.pl>
2025-06-12 23:08:33 +0200

    Update tctest.ps1 git messages

--- tctest.ps1
+++ tctest.ps1
@@ -3,16 +3,6 @@ param(
 )
 
 Write-Host "=== TeamCity Environment Information ==="
-$changedFile = $env:system_teamcity_build_changedFiles_file
-$lines = Get-Content $changedFile
-foreach ($line in $lines) {
-  $parts = $line -split ':'
-  $filePath = $parts[0]
-  $changeType = $parts[1]
-  $revision = $parts[2]
-  Write-Host "File: $filePath, Change: $changeType, Revision: $revision"
-}
-Write-Host "=== #### ==="
 $lines = Get-Content $changedFilesFile
 foreach ($line in $lines) {
   $parts = $line -split ':'
@@ -21,3 +11,9 @@ foreach ($line in $lines) {
   $revision = $parts[2]
   Write-Host "File: $filePath, Change: $changeType, Revision: $revision"
 }
+$uniqueRevs = $lines | ForEach-Object { ($_ -split ':')[-1] } | Sort-Object -Unique
+foreach ($rev in $uniqueRevs) {
+    $msg = git log -1 --pretty=format:"%H %an: %s" $rev
+    Write-Host $msg
+}
+Write-Host "=== TeamCity Environment Information ==="
