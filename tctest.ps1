c8c77a149b732bf3985c3b5e7690fee0f7ea7aea
Dawid Bieniek <dawid.bieniek@epicode.net>
2025-06-13 13:12:06 +0200

    CU-86c3xwt1a aaasdf

--- tctest.ps1
+++ tctest.ps1
@@ -1,6 +1,7 @@
 Param(
   [string] [Parameter(Mandatory=$true)] $buildNumber,
-  [string] [Parameter(Mandatory=$true)] $changesFile
+  [string] [Parameter(Mandatory=$true)] $changesFile,
+  [string] [Parameter(Mandatory=$true)] $projectName
 )
 
 # Its for testing only
@@ -8,13 +9,30 @@ $cuApiKey = "pk_200656617_1O426MO22JSSR7YWVD4D9GL0XWX1PO78"
 $headers = @{
     'Authorization' = $cuApiKey
     'Accept'        = 'application/json'
+    'Content-Type'  = 'application/json'
+}
+$projectMap = @{
+    "InnyProjekt"   = "Nowy projekt"
+    "Tctest"  = "Starszy projekt"
+    "Takiej nazwy nie będzie"  = "SomethingElse"
 }
 
 Write-Host "=== Parameter values ==="
 Write-Host "Build number: $buildNumber"
 Write-Host "Changes file path: $changesFile"
+Write-Host "Project name: $projectName"
 Write-Host "CU Api key: $cuApiKey"
 Write-Host "Headers: $headers"
+
+$mappedName = $projectMap[$projectName]
+if (-not $mappedName) {
+	$mappedName = "Empty"
+    Write-Warning "No mapping found for project '$projectName'"
+}
+else {
+	Write-Host "Project name $mappedName"
+}
+
 
 $lines = Get-Content $changesFile
 Write-Host "`n== Changes file contents: ==`n$($lines -join "`n")"
@@ -51,7 +69,29 @@ foreach ($taskId in $cuIds) {
 		Write-Host "Task: $($response.name)"
         Write-Host "Status: $($response.status.status)"
         Write-Host "Fields: $($response.custom_fields)"
+		
+		$releaseField = $response.custom_fields | Where-Object { $_.name -eq "Release" }
+		$releaseValue = if ($releaseField -and $releaseField.value) { $releaseField.value } else { [string]::Empty }
+		$fieldId = if ($releaseField -and $releaseField.id) { $releaseField.id } else { [string]::Empty }
+        Write-Host "THE FIELD: $releaseField"
 		Write-Host "---------------"
+		
+		$value = $mappedName + " " + $buildNumber
+		Write-Host "Setting field value to: $value"
+		$body = @{
+			value = "$value"
+		} | ConvertTo-Json -Depth 2
+		
+		try {
+			$url = "https://api.clickup.com/api/v2/task/$taskId/field/$fieldId"
+			Write-Host "Sending POST request"
+			$response = Invoke-RestMethod -Method Post -Uri $url -Headers $headers -Body $body
+			Write-Host "Field updated successfully."
+		} catch {
+			Write-Warning "Failed to update field: $_"
+		}
+		
+		
     } catch {
         Write-Warning "Failed to fetch task CU-$taskId $_"
     }
