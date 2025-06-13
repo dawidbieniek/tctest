4c1d154361273429e8f324cedeb7dafeeb132e3f
dawidbieniek <dawid_b01@wp.pl>
2025-06-13 11:37:30 +0200

    [CU-123test] feat: Commit 1

--- tctest.ps1
+++ tctest.ps1
@@ -1,19 +1,27 @@
-param(
-  [string]$changedFilesFile
+Param(
+  [string] [Parameter(Mandatory=$true)] $buildNumber,
+  [string] [Parameter(Mandatory=$true)] $changesFile
 )
 
-Write-Host "=== TeamCity Environment Information ==="
+Write-Host "=== Parameter values ==="
+Write-Host "Build number: $buildNumber"
+Write-Host "Changes file path: $changesFile"
+
+Write-Host "`n== Changes file contents: =="
 $lines = Get-Content $changedFilesFile
 foreach ($line in $lines) {
-  $parts = $line -split ':'
-  $filePath = $parts[0]
-  $changeType = $parts[1]
-  $revision = $parts[2]
-  Write-Host "File: $filePath, Change: $changeType, Revision: $revision"
+  Write-Host $line
+  #$parts = $line -split ':'
+  #$filePath = $parts[0]
+  #$changeType = $parts[1]
+  #$revision = $parts[2]
+  #Write-Host "File: $filePath, Change: $changeType, Revision: $revision"
 }
+
+Write-Host "`n== File Unique revs: =="
 $uniqueRevs = $lines | ForEach-Object { ($_ -split ':')[-1] } | Sort-Object -Unique
 foreach ($rev in $uniqueRevs) {
     $msg = git log -1 --pretty=format:"%H %an: %s" $rev
     Write-Host $msg
 }
-Write-Host "=== TeamCity Environment Information ==="
+Write-Host "`n=== END ==="
