70230ee89279d5b9709652c84181e4465fe455a0
Dawid Bieniek <dawid.bieniek@epicode.net>
2025-06-13 16:25:15 +0200

    a

--- tctest.ps1
+++ tctest.ps1
@@ -1,8 +1,8 @@
-Param(
-  [string] [Parameter(Mandatory=$true)] $buildNumber,
-  [string] [Parameter(Mandatory=$true)] $changesFilePath,
-  [string] [Parameter(Mandatory=$true)] $tcProjectName,
-  [string] [Parameter(Mandatory=$true)] $cuApiKey
+param(
+  [string] [Parameter(Mandatory=$true)] $BuildNumber,
+  [string] [Parameter(Mandatory=$true)] $ChangesFilePath,
+  [string] [Parameter(Mandatory=$true)] $TcProjectName,
+  [string] [Parameter(Mandatory=$true)] $CuApiKey
 )
 
 $apiKeyErrorCode = "OAUTH_019"
@@ -12,12 +12,12 @@ $projectAlreadyIsPresentWithoutBuildNrRegex = "(?i)\b{0}\b" # 0 - projectName
 $cuIdRegex = '(?i)CU-([A-Za-z0-9]+)'
 $getTaskUrl = "https://api.clickup.com/api/v2/task/{0}" # 0 - taskId
 $getTaskHeaders = @{
-    'Authorization' = $cuApiKey
+    'Authorization' = $CuApiKey
     'Accept'        = 'application/json'
 }
 $postFieldValueUrl = "https://api.clickup.com/api/v2/task/{0}/field/{1}" # 0 - taskId, 1 - fieldId
 $postFieldValueHeaders= @{
-    'Authorization' = $cuApiKey
+    'Authorization' = $CuApiKey
     'Accept'        = 'application/json'
     'Content-Type'  = 'application/json'
 }
@@ -31,15 +31,15 @@ $projectNameMap = @{
 	"Tctest" = "Stary"
 }
 
-$projectName = $projectNameMap[$tcProjectName]
+$projectName = $projectNameMap[$TcProjectName]
 if (-not $projectName) {
-    Write-Warning "Couldn't find ${tcProjectName} in project name map"
-	$projectName = $tcProjectName
+    Write-Warning "Couldn't find ${TcProjectName} in project name map"
+	$projectName = $TcProjectName
 }
 
 # get task ids
 ## Get changes file contents
-$changedFiles = Get-Content $changesFilePath
+$changedFiles = Get-Content $ChangesFilePath
 ## Extract uniq revs
 $uniqueRevs = $changedFiles | ForEach-Object { ($_ -split ':')[-1] } | Select-Object -Unique
 ## Get commit messages from revs
@@ -59,7 +59,7 @@ $cuIds | ForEach-Object { Write-Host "- $_" }
 
 # update tasks field
 foreach ($taskId in $cuIds) {
-    $url = "https://api.clickup.com/api/v2/task/$taskId"
+    $url = $getTaskUrl -f taskId
 
     try {
         $response = Invoke-RestMethod -Method Get -Uri $url -Headers $getTaskHeaders
@@ -75,19 +75,22 @@ foreach ($taskId in $cuIds) {
 		
 		if ($releaseValue -match ($projectAlreadyIsPresentWithoutBuildNrRegex -f $projectName)) {
 			write-host "Contains just project name $releaseValue"
-			$releaseValue = $releaseValue -replace ($projectAlreadyIsPresentWithoutBuildNrRegex -f $projectName), "${projectName}: $buildNumber"
+			$releaseValue = $releaseValue -replace ($projectAlreadyIsPresentWithoutBuildNrRegex -f $projectName), "${projectName}: $BuildNumber"
 		}
 		elseif ([string]::IsNullOrWhiteSpace($releaseValue)) {
 			write-host "Is empty $releaseValue"
-			$releaseValue = "${projectName}: $buildNumber"
+			$releaseValue = "${projectName}: $BuildNumber"
 		}
 		else {
 			write-host "Contains other things $releaseValue"
-			$releaseValue = "${projectName}: $buildNumber; " + $releaseValue
+			$releaseValue = "${projectName}: $BuildNumber; " + $releaseValue
 		}
 		
 		Write-Host "($taskId) changing Release field to '$releaseValue'"
 		
+		if (-not (ClickUpFieldValue -TaskId $taskId -FieldId $fieldId -Value $releaseValue)) {
+			Write-Warning "[$taskId] Failed to set Release field."
+		}
 		
 	}
 	catch [System.Net.WebException] {        
@@ -108,25 +111,36 @@ foreach ($taskId in $cuIds) {
 		Write-Warning "Failed to fetch task: $taskId`n$_"
 	}
 }
-		
-		
-		
-		# Write-Host "Setting field value to: $value"
-		# $body = @{
-			# value = "$value"
-		# } | ConvertTo-Json -Depth 2
-		
-		# try {
-			# $url = "https://api.clickup.com/api/v2/task/$taskId/field/$fieldId"
-			# Write-Host "Sending POST request"
-			# $response = Invoke-RestMethod -Method Post -Uri $url -Headers $headers -Body $body
-			# Write-Host "Field updated successfully."
-		# } catch {
-			# Write-Warning "Failed to update field: $_"
-		# }
-		
-		
-    # } catch {
-        # Write-Warning "Failed to fetch task CU-$taskId $_"
-    # }
-# }
+
+function Set-ClickUpFieldValue {
+    param(
+        [Parameter(Mandatory)][string] $TaskId,
+        [Parameter(Mandatory)][string] $FieldId,
+        [Parameter(Mandatory)][string] $Value
+    )
+
+    $body = @{ value = $Value } | ConvertTo-Json -Depth 2
+    $url = $postFieldValueUrl -f $TaskId, $FieldId
+	
+    try {
+        Invoke-RestMethod -Method Post -Uri $url -Headers $headers -Body $body
+        Write-Host "[$TaskId] ► Updated field $FieldId → $Value"
+        return $true
+    }
+    catch [System.Net.WebException] {
+        $stream = $_.Exception.Response.GetResponseStream()
+        $responseBody = [System.IO.StreamReader]::new($stream).ReadToEnd()
+        if ($responseBody) {
+            $err = ($responseBody | ConvertFrom-Json)
+            Write-Warning "[$TaskId] API error: $($err.err) (ECODE: $($err.ECODE))"
+        }
+        else {
+            Write-Warning "[$TaskId] HTTP error: $($_.Exception.Message)"
+        }
+        return $false
+    }
+    catch {
+        Write-Warning "[$TaskId] Unexpected error: $($_.Exception.Message)"
+        return $false
+    }
+}
